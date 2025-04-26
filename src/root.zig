40234cfa17d98e792fdd88262ee31c25747bf997
nat3 <nat3Github>
2025-04-26 11:52:02 +0200

    ok

--- src/root.zig
+++ src/root.zig
@@ -1,16 +1,17 @@
 const std = @import("std");
 
-pub const Image = @import("common/img2d.zig");
 pub const decoder = @import("decode/tile-decoder.zig");
 pub const maptiler = @import("decode/maptiler.zig");
 pub const Renderer = @import("render/renderer1.zig");
+pub const RendererBW = @import("render/renderer-bw.zig");
 pub const Color = @import("render/color.zig");
 pub const thickness = @import("render/thickness.zig");
 pub const Tailwind = @import("tailwind");
 
 test "all" {
     _ = .{
-        Renderer,
+        // Renderer,
+        // RendererBW,
         // maptiler,
     };
 }
